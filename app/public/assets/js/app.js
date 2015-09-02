50caaa1e9dd1969580367661b8638af39fcdc07f
Marco Paulo <markoskt@gmail.com>
2015-09-02 09:38:42 -0300

    feat: add droddown

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -5179,6 +5179,42 @@ t=null!=a.nsecs?a.nsecs:this._lastNSecs+1,w=k-this._lastMSecs+(t-this._lastNSecs
 	angular.module('cm.widgets', []);
 
 }());
+;(function count() {
+	'use strict';
+
+	function cmDropdown($window) {
+
+		function link($scope, element, attrs) {
+			var	isBinded = false;
+
+			$scope.open = function open(e) {
+				element.toggleClass('active');
+				e.stopPropagation();
+			};
+
+			element.find('ul').on('click', function(e) {
+				e.stopPropagation();
+			});
+
+			angular.element(document).on('click', function(e) {
+				if (element.hasClass('active')) element.removeClass('active');
+			});
+		}
+
+		return {
+			restrict: 'A',
+			scope: true,
+			link: link,
+		};
+	}
+
+	cmDropdown.$inject = ['$window'];
+
+	angular
+		.module('cm.widgets')
+		.directive('cmDropdown', cmDropdown);
+
+}());
 ;(function module() {
 	'use strict';
 
